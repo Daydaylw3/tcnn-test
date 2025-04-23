97205977afa96dbd143d7a50d7defc57567b328f
Daydaylw3 <daydaylw3@qq.com>
2025-04-23 09:45:05 +0800

    1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tcnn-test
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+type Task struct {
+}
+
+func (t Task) SimulateWork(id int) {
+	fmt.Println("work")
+}
+
+var maxGoroutine = 100
+
+func init() {
+	mg, _ := strconv.Atoi(os.Getenv("MAX_GOROUTINE"))
+	if mg > 0 {
+		maxGoroutine = mg
+	}
+}
+
+func main() {
+	curr := make(chan struct{}, maxGoroutine)
+	task := &Task{}
+	http.HandleFunc("/work", func(w http.ResponseWriter, r *http.Request) {
+		var wg sync.WaitGroup
+		curr <- struct{}{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			method := reflect.ValueOf(task).MethodByName("SimulateWork")
+			if method.IsValid() {
+				method.Call([]reflect.Value{reflect.ValueOf(1)})
+			}
+			<-curr
+		}()
+		wg.Wait()
+	})
+	server := &http.Server{
+		Addr: ":9090",
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
