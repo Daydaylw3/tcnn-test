3758d41bb5aa8062263cc3ccb6eafa16d5fd6c23
Daydaylw3 <daydaylw3@qq.com>
2025-04-28 21:15:00 +0800

    busy，同一个请求开启多个任务

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"reflect"
 	"runtime"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -22,12 +23,18 @@ func (t Task) SimulateWork(id int) {
 
 var maxGoroutine = 100
 
+var allocOpen bool
+
 func init() {
 	mg, _ := strconv.Atoi(os.Getenv("MAX_GOROUTINE"))
 	if mg > 0 {
 		maxGoroutine = mg
 	}
-	log.Printf("Max Goroutine: %d", mg)
+	log.Printf("Max Goroutine: %d", maxGoroutine)
+	if t := strings.ToLower(os.Getenv("ALLOC_OPEN")); t == "1" || t == "t" || t == "true" {
+		allocOpen = true
+	}
+	log.Printf("Alloc Open: %t", allocOpen)
 }
 
 func main() {
@@ -65,7 +72,30 @@ func main() {
 	server := &http.Server{
 		Addr: ":9090",
 	}
-	go simulateAlloc()
+	http.HandleFunc("/busy/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		if len(paths) < 3 {
+			http.Error(w, "xxx", http.StatusBadRequest)
+		}
+		atoi, _ := strconv.Atoi(paths[2])
+		if atoi == 0 {
+			atoi = 10
+		}
+		var wg sync.WaitGroup
+		for i := 0; i < atoi; i++ {
+			wg.Add(1)
+			curr <- struct{}{}
+			go func() {
+				defer wg.Done()
+				doBusy()
+				<-curr
+			}()
+		}
+		wg.Wait()
+	})
+	if allocOpen {
+		go simulateAlloc()
+	}
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
@@ -82,5 +112,38 @@ func simulateAlloc() {
 		fmt.Printf("Allocated large object with address: %p\n", largeObject)
 
 		time.Sleep(time.Second * 3)
+	}
+}
+
+var mutex sync.Mutex
+var funcTypes []reflect.Type
+
+func doThing(n int) reflect.Type {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if n >= len(funcTypes) {
+		newFuncTypes := make([]reflect.Type, n+1)
+		copy(newFuncTypes, funcTypes)
+		funcTypes = newFuncTypes
+	}
+	if funcTypes[n] != nil {
+		return funcTypes[n]
+	}
+	funcTypes[n] = reflect.StructOf([]reflect.StructField{
+		{
+			Name: "FuncType",
+			Type: reflect.TypeOf(""),
+		},
+		{
+			Name: "Args",
+			Type: reflect.ArrayOf(n, reflect.TypeOf("")),
+		},
+	})
+	return funcTypes[n]
+}
+
+func doBusy() {
+	for i := 0; i < 128; i++ {
+		doThing(i)
 	}
 }
