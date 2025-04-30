179672277e62a37445ab39a1019d44a2b6ee845f
Daydaylw3 <daydaylw3@qq.com>
2025-04-30 09:37:18 +0800

    重构一下，增加了 ctx cancel，增加busy任务打印

---
+++ handle_func.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var curr chan struct{}
+
+var task = &Task{}
+
+func work(_ http.ResponseWriter, _ *http.Request) {
+	var wg sync.WaitGroup
+	curr <- struct{}{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		method := reflect.ValueOf(task).MethodByName("SimulateWork")
+		if method.IsValid() {
+			method.Call([]reflect.Value{reflect.ValueOf(1)})
+		}
+		<-curr
+	}()
+	wg.Wait()
+}
+
+func mutexFunc() http.HandlerFunc {
+	var (
+		counter      int
+		counterMutex sync.Mutex
+	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		counterMutex.Lock()
+		defer counterMutex.Unlock()
+
+		counter++
+		fmt.Fprintf(w, "Counter: %d\n", counter)
+	}
+}
+
+func busy() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		taskC := 10000
+		if len(paths) >= 3 {
+			if i, _ := strconv.Atoi(paths[2]); i > 0 {
+				taskC = i
+			}
+		}
+		start := time.Now()
+
+		if done, err := assignTask(r.Context(), taskC); err != nil {
+			cost := time.Since(start).Round(time.Millisecond)
+			log.Printf("busy job canceled, %10d tasks done, cost: %s", done, cost)
+			return
+		}
+		cost := time.Since(start).Round(time.Millisecond)
+		log.Printf("busy job finish, %10d tasks done, cost: %s", taskC, cost)
+		fmt.Fprintf(w, "busy job finish, %10d tasks done, cost: %s\n", taskC, cost)
+	}
+}
+
+func assignTask(ctx context.Context, taskC int) (int, error) {
+	var wg sync.WaitGroup
+	for i := 0; i < taskC; i++ {
+		wg.Add(1)
+		curr <- struct{}{}
+		select {
+		case <-ctx.Done():
+			return i + 1, ctx.Err()
+		default:
+			go func() {
+				defer wg.Done()
+				doBusy(ctx)
+				<-curr
+			}()
+		}
+	}
+	wg.Wait()
+	return taskC, ctx.Err()
+}
+
+var mutex sync.Mutex
+var funcTypes []reflect.Type
+
+func doThing(n int) reflect.Type {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if n >= len(funcTypes) {
+		newFuncTypes := make([]reflect.Type, n+1)
+		copy(newFuncTypes, funcTypes)
+		funcTypes = newFuncTypes
+	}
+	if funcTypes[n] != nil {
+		return funcTypes[n]
+	}
+	funcTypes[n] = reflect.StructOf([]reflect.StructField{
+		{
+			Name: "FuncType",
+			Type: reflect.TypeOf(""),
+		},
+		{
+			Name: "Args",
+			Type: reflect.ArrayOf(n, reflect.TypeOf("")),
+		},
+	})
+	return funcTypes[n]
+}
+
+func doBusy(ctx context.Context) error {
+	for i := 0; i < 128; i++ {
+		select {
+		case <-ctx.Done():
+			// 如果上下文被取消，退出并返回错误
+			return ctx.Err()
+		default:
+			// 没有取消信号，进行正常的工作
+			doThing(i)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -6,11 +6,9 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -36,6 +34,7 @@ func init() {
 		maxGoroutine = mg
 	}
 	log.Printf("Max Goroutine: %d", maxGoroutine)
+	curr = make(chan struct{}, maxGoroutine)
 	if t := strings.ToLower(os.Getenv("ALLOC_OPEN")); t == "1" || t == "t" || t == "true" {
 		allocOpen = true
 	}
@@ -47,98 +46,19 @@ func main() {
 	runtime.SetBlockProfileRate(1)
 	runtime.SetMutexProfileFraction(1)
 
-	curr := make(chan struct{}, maxGoroutine)
-	task := &Task{}
-	http.HandleFunc("/work", func(w http.ResponseWriter, r *http.Request) {
-		var wg sync.WaitGroup
-		curr <- struct{}{}
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			method := reflect.ValueOf(task).MethodByName("SimulateWork")
-			if method.IsValid() {
-				method.Call([]reflect.Value{reflect.ValueOf(1)})
-			}
-			<-curr
-		}()
-		wg.Wait()
-	})
-	var (
-		counter      int
-		counterMutex sync.Mutex
-	)
-	http.HandleFunc("/mutex", func(w http.ResponseWriter, r *http.Request) {
-		counterMutex.Lock()
-		defer counterMutex.Unlock()
-
-		counter++
-		fmt.Fprintf(w, "Counter: %d\n", counter)
-	})
+	http.HandleFunc("/work", work)
+	http.HandleFunc("/mutex", mutexFunc())
+	http.HandleFunc("/busy/", busy())
+	http.HandleFunc("/busy", busy())
 	server := &http.Server{
 		Addr: endpoint,
 	}
-	http.HandleFunc("/busy/", func(w http.ResponseWriter, r *http.Request) {
-		paths := strings.Split(r.URL.Path, "/")
-		if len(paths) < 3 {
-			http.Error(w, "xxx", http.StatusBadRequest)
-		}
-		atoi, _ := strconv.Atoi(paths[2])
-		if atoi == 0 {
-			atoi = 10
-		}
-		start := time.Now()
-		var wg sync.WaitGroup
-		for i := 0; i < atoi; i++ {
-			wg.Add(1)
-			curr <- struct{}{}
-			go func() {
-				defer wg.Done()
-				doBusy()
-				<-curr
-			}()
-		}
-		wg.Wait()
-		fmt.Fprintf(w, "cost: %s\n", time.Since(start).Round(time.Millisecond*1))
-	})
 	if allocOpen {
 		go simulateAlloc()
 	}
 	log.Printf("Server start at: %s", endpoint)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
-	}
-}
-
-var mutex sync.Mutex
-var funcTypes []reflect.Type
-
-func doThing(n int) reflect.Type {
-	mutex.Lock()
-	defer mutex.Unlock()
-	if n >= len(funcTypes) {
-		newFuncTypes := make([]reflect.Type, n+1)
-		copy(newFuncTypes, funcTypes)
-		funcTypes = newFuncTypes
-	}
-	if funcTypes[n] != nil {
-		return funcTypes[n]
-	}
-	funcTypes[n] = reflect.StructOf([]reflect.StructField{
-		{
-			Name: "FuncType",
-			Type: reflect.TypeOf(""),
-		},
-		{
-			Name: "Args",
-			Type: reflect.ArrayOf(n, reflect.TypeOf("")),
-		},
-	})
-	return funcTypes[n]
-}
-
-func doBusy() {
-	for i := 0; i < 128; i++ {
-		doThing(i)
 	}
 }
 
@@ -151,7 +71,7 @@ func simulateAlloc() {
 		largeObject := &largeObject{}
 
 		_ = largeObject
-		//fmt.Printf("Allocated large object with address: %p\n", largeObject)
+		// fmt.Printf("Allocated large object with address: %p\n", largeObject)
 
 		time.Sleep(time.Second * 3)
 	}
